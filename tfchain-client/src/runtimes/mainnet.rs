c47af94d24a795903fe62b2bda84c7c8ba26145e
dylanverstraete <dylan_verstraete1@hotmail.com>
2023-03-27 10:16:56 +0200

    fix: make it compile

--- tfchain-client/src/runtimes/mainnet.rs
+++ tfchain-client/src/runtimes/mainnet.rs
@@ -2,7 +2,7 @@ use super::types::*;
 use crate::{Client, KeyPair};
 use subxt::{subxt, Error};
 
-#[subxt(runtime_metadata_path = "artifacts/mainnet.scale")]
+#[subxt(runtime_metadata_path = "artifacts/mainnet.scale", derive_for_all_types = "Eq, PartialEq")]
 pub mod mainnet {}
 pub use mainnet::runtime_types::frame_system::AccountInfo;
 
@@ -33,17 +33,21 @@ pub async fn get_balance(
 		.map(|t| GenericAccountInfo::from(t)))
 }
 
+// Wrapper around the transfer function of the balances pallet
+// This function will wait for the transaction to be finalized and return the block hash
+// If the transaction fails, it will return an error
+// If the transaction is successful, it will return the block hash
 pub async fn transfer_native(
 	cl: &Client,
 	kp: &KeyPair,
 	dest: AccountId32,
-	value: u128,
+	amount: u128,
 ) -> Result<Hash, Error> {
-	let transfer_tx = mainnet::tx().balances().transfer(dest.into(), value);
+	let transfer_tx = mainnet::tx().balances().transfer(dest.clone().into(), amount);
 
 	let s = &kp.signer();
 
-	let transfer = cl
+	let events = cl
 		.api
 		.tx()
 		.sign_and_submit_then_watch_default(&transfer_tx, s)
@@ -51,10 +55,18 @@ pub async fn transfer_native(
 		.wait_for_finalized_success()
 		.await?;
 
-	let balances_transfer = transfer.find_first::<mainnet::balances::events::Transfer>()?;
+	let expected_event = mainnet::balances::events::Transfer {
+		from: s.0.account_id().clone(),
+		to: dest.into(),
+		amount,
+	};
 
-	if let Some(_) = balances_transfer {
-		Ok(transfer.block_hash())
+	let exists = events
+		.find::<mainnet::balances::events::Transfer>()
+		.any(|e| e.map(|x| assert_eq!(x, expected_event)).is_ok());
+
+	if exists {
+		Ok(events.block_hash())
 	} else {
 		Err(Error::Other(String::from("failed to transfer")))
 	}
